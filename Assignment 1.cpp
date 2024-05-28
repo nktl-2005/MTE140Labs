454601b6b775f4c9048de3a450f9148103f6bd15
Riya Bharadia <riyabharadia@hotmail.com>
2024-05-28 18:28:13 -0400

    starting the class

--- Assignment 1.cpp
+++ Assignment 1.cpp
@@ -1,2 +1,12 @@
 //Hi
 //hello
+
+#include <iostream>
+using namespace std;
+
+class CircularLinkedList { //making the class
+    int value; //setting the data type as integer
+
+    CircularLinkedList* next; //assigning each node to go to the next one via pointer
+
+};
