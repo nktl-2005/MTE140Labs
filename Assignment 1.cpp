51da8307552115c10de69d43cb727c06dcd63687
Nick <nicholasleekt@gmail.com>
2024-06-01 13:39:55 -0400

    added display

--- Assignment 1.cpp
+++ Assignment 1.cpp
@@ -1,2 +1,42 @@
-//Hi
-//hello
+//
+#include <iostream>
+using namespace std;
+class CircularLinkedList {
+public:
+    int value;
+    CircularLinkedList *next;
+
+    void insertion(){
+
+    }
+
+    void deletion(){
+
+    }
+
+    void display(CircularLinkedList* head){
+        if (head == nullptr) {
+        cout << "List is empty" << endl;
+        return;
+        }
+        CircularLinkedList* temp = head;
+        do {
+        cout<< temp->value <<" ";
+        temp = temp->next;
+        } while (temp != head);
+        cout << endl;
+    }
+
+    void search(){
+
+    }
+};
+typedef CircularLinkedList* Node;
+int main() {
+    Node *head = new Node();
+    Node *second = new Node();
+
+
+
+    return 0;
+}
