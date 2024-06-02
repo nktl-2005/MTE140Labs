f7af3645e380940604a02c323246980c80037f28
Riya Bharadia <riyabharadia@hotmail.com>
2024-06-02 12:17:05 -0400

    added deletion

--- Assignment 1.cpp
+++ Assignment 1.cpp
@@ -3,15 +3,67 @@
 using namespace std;
 class CircularLinkedList {
 public:
-    int value;
-    CircularLinkedList *next;
+    struct Node {
+        int data;
+        Node* next;
 
-    void insertion(){
+        Node(int value) : data(value), next(nullptr) {}
+    };
 
-    }
+    Node* head;
+    void insertion(){}
+    
+    void deletion(CircularLinkedList* head, int delete_number){
+        // in the case that the list is empty
+        if (head==nullptr || !head) {
+            cout << "List is empty and therefore the number is not in the list" << endl;
+            return;
+        }
+        // initizaiting these pointers to changge once the delete number is found
+        Node* curr = head;
+        Node* prev = nullptr;
+        Node* lastOccur = nullptr;
+        Node* lastOccurPrev = nullptr;
 
-    void deletion(){
+       // setting values to last node to work backwards
+       do {
+            if (curr->data = delete_number){
+                lastOccur = curr;
+                lastOccurPrev = prev;
+            }
+            //setting up to reassign pointers
+            prev = curr;
+            curr = curr->next;
+       } while (curr !=head);
 
+        // number not found
+        if (!lastOccur) {
+            cout << "Number provided is not in the list" << endl;
+        }
+
+        // if the number is the head node
+        if (lastOccur == head) {
+            if (head->next == head) {
+                delete head;
+                head = nullptr;
+            }
+            else { //there is more than 1 node in the list
+                Node* temp = head;
+                while (temp->next !=head) {
+                    temp = temp->next;
+                }
+                // resigning the pointers and deleting the entry correctly
+                temp->next = head->next;
+                Node* toDelete = head;
+                head = head->next;
+                delete toDelete;
+            } 
+        }
+        else {
+            // if the number is not the head node
+            lastOccurPrev->next = lastOccur->next;
+            delete lastOccur;
+        }
     }
 
     void display(CircularLinkedList* head){
