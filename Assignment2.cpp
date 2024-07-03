bc5f5a19ab527bda583801d5fa4e152c4b738177
Riya Bharadia <riyabharadia@hotmail.com>
2024-07-03 18:29:03 -0400

    assignment 2

---
+++ Assignment2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+// isSafe function (bool) to see if the position is safe to place a queen
+
+// Solution (bool?) returns if the solution is possible and solves it
+
+// PrintSol function (void) to print the solution using Solution function
+
+main() {} 
